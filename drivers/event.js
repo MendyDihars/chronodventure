cf248102c392fdc3c722c01e27f17d0ccb6b06df
Mendy Dihars <mendy.dihars@laposte.net>
2020-04-25 13:35:58 +0200

    change query

--- drivers/event.js
+++ drivers/event.js
@@ -3,7 +3,7 @@ import { Event } from '../data/db';
 export default class EventDriver {
     static getEvents() {
         return new Promise((resolve, reject) => {
-            Event.find({}, (err, data) => {
+            Event.find({}).sort({ position: 1 }).exec((err, data) => {
                 if (err) reject(err);
                 resolve(data);
             })
